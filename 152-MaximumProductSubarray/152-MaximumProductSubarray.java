7775935b13f0fe3b04dc9d8a1d9de4e66da06e3a
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-04 00:58:30 +0530

    Create 152-MaximumProductSubarray

---
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.java
@@ -0,0 +1,18 @@
+// Last updated: 8/4/2025, 12:58:40 AM
+class Solution {
+    public int maxProduct(int[] nums) {
+        int prefix=1;
+        int suffix=1;
+        int ans=nums[0];
+        int n=nums.length;
+        double mm=1e9;
+        for (int i=0;i<n;i++){
+            prefix=prefix==0 ? 1:prefix;
+            suffix=suffix==0?1:suffix;
+            prefix=prefix*nums[i];
+            suffix=suffix*nums[n-i-1];
+            ans=(int)Math.max(ans,Math.max(prefix,suffix));
+        }
+        return ans;
+    }
+}
