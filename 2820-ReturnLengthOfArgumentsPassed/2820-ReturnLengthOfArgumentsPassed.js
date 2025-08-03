15606bb8f770e69b48a28729efd832664bc30b78
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-04 00:56:44 +0530

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,11 @@
+// Last updated: 8/4/2025, 12:56:54 AM
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+ return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
