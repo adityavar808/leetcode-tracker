785b08bda20a1eff8ea232e87cba5b99c2034516
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-04 00:57:09 +0530

    Create 1993-SumOfAllSubsetXorTotals

---
+++ 1993-SumOfAllSubsetXorTotals/1993-SumOfAllSubsetXorTotals.java
@@ -0,0 +1,15 @@
+// Last updated: 8/4/2025, 12:57:20 AM
+class Solution {
+  public int subsetXORSum(int[] nums) {
+    return dfs(nums, 0, 0);
+  }
+
+  private int dfs(int[] nums, int i, int xors) {
+    if (i == nums.length)
+      return xors;
+
+    final int x = dfs(nums, i + 1, xors);
+    final int y = dfs(nums, i + 1, nums[i] ^ xors);
+    return x + y;
+  }
+}
