efbd69846720a5a12ffb5a8f530c5d646ecac1ca
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-04 00:56:46 +0530

    Create 2707-MergeTwo2dArraysBySummingValues

---
+++ 2707-MergeTwo2dArraysBySummingValues/2707-MergeTwo2dArraysBySummingValues.java
@@ -0,0 +1,25 @@
+// Last updated: 8/4/2025, 12:56:56 AM
+class Solution {
+  public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+    final int MAX = 1000;
+    List<int[]> ans = new ArrayList<>();
+    int[] count = new int[MAX + 1];
+
+    addCount(nums1, count);
+    addCount(nums2, count);
+
+    for (int i = 1; i <= MAX; ++i)
+      if (count[i] > 0)
+        ans.add(new int[] {i, count[i]});
+
+    return ans.stream().toArray(int[][] ::new);
+  }
+
+  private void addCount(int[][] nums, int[] count) {
+    for (int[] idAndVal : nums) {
+      final int id = idAndVal[0];
+      final int val = idAndVal[1];
+      count[id] += val;
+    }
+  }
+}
