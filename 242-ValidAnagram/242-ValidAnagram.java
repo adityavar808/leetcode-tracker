94d16174254e67fae50d9363a4f4f978e9cb2719
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-04 00:58:03 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,14 @@
+// Last updated: 8/4/2025, 12:58:13 AM
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
+    }
+}
