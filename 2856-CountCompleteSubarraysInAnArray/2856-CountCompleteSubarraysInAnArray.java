961f59697ed67c64b9209fdbd0b95fd476fe6be7
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-04 00:56:43 +0530

    Create 2856-CountCompleteSubarraysInAnArray

---
+++ 2856-CountCompleteSubarraysInAnArray/2856-CountCompleteSubarraysInAnArray.java
@@ -0,0 +1,21 @@
+// Last updated: 8/4/2025, 12:56:53 AM
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for (int x : nums) {
+            s.add(x);
+        }
+        int cnt = s.size();
+        int ans = 0, n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            s.clear();
+            for (int j = i; j < n; ++j) {
+                s.add(nums[j]);
+                if (s.size() == cnt) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
