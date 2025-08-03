02bb3076266a096c65416f878ba201dfa3a7c333
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-04 00:56:52 +0530

    Create 2503-LongestSubarrayWithMaximumBitwiseAnd

---
+++ 2503-LongestSubarrayWithMaximumBitwiseAnd/2503-LongestSubarrayWithMaximumBitwiseAnd.java
@@ -0,0 +1,21 @@
+// Last updated: 8/4/2025, 12:57:02 AM
+class Solution {
+  public int longestSubarray(int[] nums) {
+    int ans = 0;
+    int maxIndex = 0;
+    int sameNumLength = 0;
+
+    for (int i = 0; i < nums.length; ++i)
+      if (nums[i] == nums[maxIndex]) {
+        ans = Math.max(ans, ++sameNumLength);
+      } else if (nums[i] > nums[maxIndex]) {
+        maxIndex = i;
+        sameNumLength = 1;
+        ans = 1;
+      } else {
+        sameNumLength = 0;
+      }
+
+    return ans;
+  }
+}
