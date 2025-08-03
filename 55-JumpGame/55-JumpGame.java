42fc7542c73c0b4415d2188dd3aaf77cb2c98a45
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-04 00:59:34 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,11 @@
+// Last updated: 8/4/2025, 12:59:44 AM
+class Solution {
+  public boolean canJump(int[] nums) {
+    int i = 0;
+
+    for (int reach = 0; i < nums.length && i <= reach; ++i)
+      reach = Math.max(reach, i + nums[i]);
+
+    return i == nums.length;
+  }
+}
