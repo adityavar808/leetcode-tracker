8e610076d5c1eab23bfd41a81ea523426caa53fc
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-11 22:45:54 +0530

    File updated at 8/11/2025, 10:46:07 PM

--- 87-ScrambleString/87-ScrambleString.java
+++ 87-ScrambleString/87-ScrambleString.java
@@ -1,51 +1,22 @@
-// Last updated: 8/11/2025, 10:45:30 PM
-class Solution {
-    Map<String, Boolean> mp = new HashMap<>();
+// Last updated: 8/11/2025, 10:46:07 PM
+public class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+    
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
 
-    public boolean isScramble(String s1, String s2) {
-        int n = s1.length();
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
 
-        if (s2.length() != n)
-            return false;
-
-        if (s1.equals(s2))
-            return true;
-
-        if (n == 1)
-            return false;
-
-        String key = s1 + " " + s2;
-
-        if (mp.containsKey(key))
-            return mp.get(key);
-
-        for (int i = 1; i < n; i++) {
-            boolean withoutswap = (
-                    isScramble(s1.substring(0, i), s2.substring(0, i))
-
-                            &&
-
-                            isScramble(s1.substring(i), s2.substring(i))
-            );
-
-            if (withoutswap) {
-                mp.put(key, true);
-                return true;
-            }
-
-            boolean withswap = (
-                    isScramble(s1.substring(0, i), s2.substring(n - i))
-
-                            &&
-
-                            isScramble(s1.substring(i), s2.substring(0, n - i))
-            );
-            if (withswap) {
-                mp.put(key, true);
-                return true;
-            }
-        }
-        mp.put(key, false);
-        return false;
+    public int next() {
+        TreeNode tmpNode = stack.pop();
+        pushAll(tmpNode.right);
+        return tmpNode.val;
+    }
+    
+    private void pushAll(TreeNode node) {
+        for (; node != null; stack.push(node), node = node.left);
     }
 }
