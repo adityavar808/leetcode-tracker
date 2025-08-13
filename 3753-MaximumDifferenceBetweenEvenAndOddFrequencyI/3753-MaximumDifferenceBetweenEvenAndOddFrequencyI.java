2bcf72166c9339e16a21d3770a4a6abc7aca4bd0
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-13 12:17:57 +0530

    Create 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI

---
+++ 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI/3753-MaximumDifferenceBetweenEvenAndOddFrequencyI.java
@@ -0,0 +1,24 @@
+// Last updated: 8/13/2025, 12:18:10 PM
+class Solution {
+  public int maxDifference(String s) {
+    int[] count = new int[26];
+    int maxOdd = 0;
+    int minEven = s.length();
+
+    for (final char c : s.toCharArray())
+      ++count[c - 'a'];
+
+    for (final int freq : count) {
+      if (freq == 0)
+        continue;
+      if (freq % 2 == 0)
+        minEven = Math.min(minEven, freq);
+      else
+        maxOdd = Math.max(maxOdd, freq);
+    }
+
+    return maxOdd - minEven;
+  }
+}
+
+
