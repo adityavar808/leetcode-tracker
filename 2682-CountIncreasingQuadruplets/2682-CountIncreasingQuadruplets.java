40c2bd736d713c53af7912f72690e9249decbd9b
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-13 12:18:12 +0530

    Create 2682-CountIncreasingQuadruplets

---
+++ 2682-CountIncreasingQuadruplets/2682-CountIncreasingQuadruplets.java
@@ -0,0 +1,20 @@
+// Last updated: 8/13/2025, 12:18:25 PM
+class Solution {
+    public long countQuadruplets(int[] nums) {
+        int n = nums.length;
+        long[] cnt = new long[n];
+        long ans = 0;
+        for (int j = 0; j < n; j++) {
+            int prev_small = 0;
+            for (int i = 0; i < j; i++) {
+                if (nums[j] > nums[i]) {
+                    prev_small++;
+                    ans += cnt[i];
+                } else if (nums[j] < nums[i]) {
+                    cnt[i] += prev_small;
+                }
+            }
+        }
+        return ans;
+    }
+}
