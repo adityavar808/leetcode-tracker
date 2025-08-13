14b1f26d9af138668a8ee8cd3b65caedbb0b2c40
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-13 12:19:49 +0530

    Create 173-BinarySearchTreeIterator

---
+++ 173-BinarySearchTreeIterator/173-BinarySearchTreeIterator.java
@@ -0,0 +1,22 @@
+// Last updated: 8/13/2025, 12:20:03 PM
+public class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+    
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    public int next() {
+        TreeNode tmpNode = stack.pop();
+        pushAll(tmpNode.right);
+        return tmpNode.val;
+    }
+    
+    private void pushAll(TreeNode node) {
+        for (; node != null; stack.push(node), node = node.left);
+    }
+}
