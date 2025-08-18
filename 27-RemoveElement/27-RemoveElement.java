0ed2923c6e39b91894978e2741cb40ec0fab4b39
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-18 21:29:13 +0530

    File updated at 8/18/2025, 9:29:28 PM

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,13 +1,13 @@
-// Last updated: 8/4/2025, 1:00:13 AM
+// Last updated: 8/18/2025, 9:29:28 PM
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[index] = nums[i];
-                index++;
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return index;
+        return k;
     }
 }
