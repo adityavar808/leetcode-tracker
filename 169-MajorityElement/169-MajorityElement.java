0de885289c4dc412afc4b2946eeac51986c0f37a
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-20 10:19:29 +0530

    File updated at 8/20/2025, 10:19:45 AM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,22 +1,9 @@
-// Last updated: 8/4/2025, 12:58:35 AM
+// Last updated: 8/20/2025, 10:19:45 AM
 class Solution {
     public int majorityElement(int[] nums) {
-        return moore_voting(nums);
+        Arrays.sort(nums);
+        int arrLength = nums.length;
+        int midIdx = arrLength / 2;
+        return nums[midIdx];
     }
-    public static int moore_voting(int[] arr) {
-		int e = arr[0];
-		int v = 1;
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] == e) {
-				v++;
-			} else {
-				v--;
-				if (v == 0) {
-					e = arr[i];
-					v = 1;
-				}
-			}
-		}
-		return e;
-	}
 }
