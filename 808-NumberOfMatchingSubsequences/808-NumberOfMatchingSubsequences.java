55b214c847f78105ef5dec6e44a8db8ce20146cf
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-28 15:17:02 +0530

    Create 808-NumberOfMatchingSubsequences

---
+++ 808-NumberOfMatchingSubsequences/808-NumberOfMatchingSubsequences.java
@@ -0,0 +1,26 @@
+// Last updated: 8/28/2025, 3:17:04 PM
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+        Map<Character, Queue<Pair<String, Integer>>> buckets = new HashMap<>();
+        for (char ch = 'a'; ch <= 'z'; ch++)
+            buckets.put(ch, new LinkedList<>());
+
+        for (String word : words)
+            buckets.get(word.charAt(0)).offer(new Pair<>(word, 0));
+
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            Queue<Pair<String, Integer>> queue = buckets.get(c);
+            int size = queue.size();
+            while (size-- > 0) {
+                Pair<String, Integer> pair = queue.poll();
+                String word = pair.getKey();
+                int idx = pair.getValue() + 1;
+                if (idx == word.length()) count++;
+                else buckets.get(word.charAt(idx)).offer(new Pair<>(word, idx));
+            }
+        }
+
+        return count;
+    }
+}
