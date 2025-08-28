2739ff22361de64a9123769d7af4085323b2f0a1
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-29 00:05:35 +0530

    File updated at 8/29/2025, 12:05:37 AM

--- 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.java
+++ 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.java
@@ -1,7 +1,31 @@
-// Last updated: 8/29/2025, 12:02:09 AM
-public class Solution {
-    public boolean isPowerOfThree(int n) {
-        int maxPowerOf3 = 1162261467; // 3^19 is the largest power of 3 in int range
-        return n > 0 && maxPowerOf3 % n == 0;
+// Last updated: 8/29/2025, 12:05:37 AM
+class Solution {
+    private Double[][] cache; 
+
+    public double soupServings(int n) {
+        if (n > 5000) return 1.0;
+
+        int units = (int) Math.ceil(n / 25.0); 
+        cache = new Double[units + 1][units + 1];
+
+        return calcProb(units, units);
+    }
+
+    private double calcProb(int soupA, int soupB) {
+        if (soupA <= 0 && soupB <= 0) return 0.5;
+        if (soupA <= 0) return 1.0;
+        if (soupB <= 0) return 0.0;
+
+        if (cache[soupA][soupB] != null) return cache[soupA][soupB];
+
+        double prob = 0.25 * (
+            calcProb(soupA - 4, soupB) +
+            calcProb(soupA - 3, soupB - 1) +
+            calcProb(soupA - 2, soupB - 2) +
+            calcProb(soupA - 1, soupB - 3)
+        );
+
+        cache[soupA][soupB] = prob;
+        return prob;
     }
 }
