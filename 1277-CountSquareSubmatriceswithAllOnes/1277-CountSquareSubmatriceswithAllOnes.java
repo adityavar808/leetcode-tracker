9cc7a0569caed7203cfe723e6c2eb111fc0e6838
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-28 23:25:54 +0530

    File updated at 8/28/2025, 11:25:56 PM

--- 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.java
+++ 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.java
@@ -1,26 +1,42 @@
-// Last updated: 8/28/2025, 11:25:31 PM
+// Last updated: 8/28/2025, 11:25:56 PM
 class Solution {
-    public int countSquares(int[][] A) {
-        if (A == null || A.length == 0 || A[0].length == 0) {
-            return 0;
+    final double EPS = 1e-6;
+
+    public boolean judgePoint24(int[] cards) {
+        List<Double> nums = new ArrayList<>();
+        for (int n : cards) nums.add((double) n);
+        return dfs(nums);
+    }
+
+    private boolean dfs(List<Double> nums) {
+        if (nums.size() == 1) {
+            return Math.abs(nums.get(0) - 24.0) < EPS;
         }
-
-        int m = A.length;
-        int n = A[0].length;
-        int res = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (A[i][j] == 1 && i > 0 && j > 0) {
-                    A[i][j] = Math.min(
-                        A[i - 1][j - 1],
-                        Math.min(A[i - 1][j], A[i][j - 1])
-                    ) + 1;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if (i == j) continue;
+                List<Double> next = new ArrayList<>();
+                for (int k = 0; k < nums.size(); k++) {
+                    if (k != i && k != j) next.add(nums.get(k));
                 }
-                res += A[i][j];
+                for (double val : compute(nums.get(i), nums.get(j))) {
+                    next.add(val);
+                    if (dfs(next)) return true;
+                    next.remove(next.size() - 1);
+                }
             }
         }
+        return false;
+    }
 
+    private List<Double> compute(double a, double b) {
+        List<Double> res = new ArrayList<>();
+        res.add(a + b);
+        res.add(a - b);
+        res.add(b - a);
+        res.add(a * b);
+        if (Math.abs(b) > EPS) res.add(a / b);
+        if (Math.abs(a) > EPS) res.add(b / a);
         return res;
     }
 }
