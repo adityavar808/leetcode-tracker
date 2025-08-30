11dfa0c8ae99e9173e0407a6f3637ef318d48606
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-30 12:44:45 +0530

    File updated at 8/30/2025, 12:44:48 PM

--- 1504-CountSubmatricesWithAllOnes/1504-CountSubmatricesWithAllOnes.java
+++ 1504-CountSubmatricesWithAllOnes/1504-CountSubmatricesWithAllOnes.java
@@ -1,39 +1,20 @@
-// Last updated: 8/30/2025, 12:44:25 PM
+// Last updated: 8/30/2025, 12:44:48 PM
 class Solution {
-    int dirs[][] = {{-1,1},{1,1},{1,-1},{-1,-1}};
-    int [][] grid;
-    int row, col;   
+    public int areaOfMaxDiagonal(int[][] dimensions) {
+        int maxArea = Integer.MIN_VALUE;
+        int maxDiagonal = Integer.MIN_VALUE;
+        int n = dimensions.length;
+        
+        for( int i = 0; i < n; i++ ){
+            int l = dimensions[i][0];
+            int w = dimensions[i][1];
+            int currDiagonal = l * l + w * w;
 
-    public int lenOfVDiagonal(int[][] grid) {
-        row = grid.length;
-        col = grid[0].length;
-        this.grid = grid;
-        int res = 0;
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(grid[i][j] == 1) {
-                    res = Math.max(res, 1);
-                    for(int d = 0; d < 4; d++) {
-                        res = Math.max(res, dfs(i, j, d, 2,false));
-                    }
-                }
+            if( currDiagonal > maxDiagonal ||  (currDiagonal == maxDiagonal && l * w > maxArea) ) {
+                maxDiagonal = currDiagonal;
+                maxArea = l * w;
             }
         }
-        return res;
-    }
-
-    private int dfs(int i, int j , int dir ,int target, boolean isTurned){
-        int x = i + dirs[dir][0];
-        int y = j + dirs[dir][1];
-
-        if(x < 0 || x >= row || y < 0 || y >= col || grid[x][y] != target) return 1;
-
-        int straight = 1 + dfs(x, y, dir, target == 2 ? 0: 2, isTurned);
-
-        int turn = 0;
-        if(!isTurned) { 
-            turn = 1 + dfs(x, y, (dir + 1) % 4, target == 2 ? 0: 2, true);
-        }
-        return Math.max(straight, turn);
+        return maxArea;
     }
 }
