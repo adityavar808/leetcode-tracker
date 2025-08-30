5495da739dad5c71eb2aecd8d9dda1f242ceb5e1
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-30 12:45:59 +0530

    Create 1695-MaximumErasureValue

---
+++ 1695-MaximumErasureValue/1695-MaximumErasureValue.java
@@ -0,0 +1,30 @@
+// Last updated: 8/30/2025, 12:46:03 PM
+public class Solution 
+{
+    public int maximumUniqueSubarray(int[] nums) 
+    {
+        int n = nums.length;
+
+        int[] prefixSum = new int[n + 1];
+        for (int i = 0; i < n; ++i)
+        {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+
+        int[] lastSeen = new int[10001];
+
+        int left = 0, maxSum = 0;
+        for (int right = 1; right <= n; ++right) 
+        {
+            int val = nums[right - 1];
+
+            left = Math.max(left, lastSeen[val]);
+
+            maxSum = Math.max(maxSum, prefixSum[right] - prefixSum[left]);
+
+            lastSeen[val] = right;
+        }
+
+        return maxSum;
+    }
+}
