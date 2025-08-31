2f24e67600fbf0c9ed3137b18ae541481506dc37
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-08-31 09:18:19 +0530

    File updated at 8/31/2025, 9:18:23 AM

--- 3195-FindtheMinimumAreatoCoverAllOnesI/3195-FindtheMinimumAreatoCoverAllOnesI.java
+++ 3195-FindtheMinimumAreatoCoverAllOnesI/3195-FindtheMinimumAreatoCoverAllOnesI.java
@@ -1,6 +1,27 @@
-// Last updated: 8/31/2025, 9:17:57 AM
+// Last updated: 8/31/2025, 9:18:23 AM
 class Solution {
-    public boolean isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+    public int[] productQueries(int n, int[][] queries) {
+        final int MOD = 1000000007;
+        List<Integer> powers = new ArrayList<>();
+        while (n > 0) {
+            int lowBit = n & -n;
+            powers.add(lowBit);
+            n ^= lowBit;
+        }
+        int size = powers.size();
+        int[][] table = new int[size][size];
+        for (int row = 0; row < size; row++) {
+            table[row][row] = powers.get(row);
+            for (int col = row + 1; col < size; col++) {
+                table[row][col] = (int)((long)table[row][col - 1] * powers.get(col) % MOD);
+            }
+        }
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int p = queries[i][0];
+            int q = queries[i][1];
+            result[i] = table[p][q];
+        }
+        return result;
     }
 }
