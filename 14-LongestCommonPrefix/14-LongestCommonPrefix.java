b92b62c25ca82e54c80966e7cfa93a053758e840
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-09-02 22:35:15 +0530

    File updated at 9/2/2025, 10:35:20 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.java
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.java
@@ -1,14 +1,18 @@
-// Last updated: 9/2/2025, 10:34:54 PM
+// Last updated: 9/2/2025, 10:35:20 PM
 class Solution {
-    public int[] constructRectangle(int area) {
-        int W = (int) Math.sqrt(area);
-        
-        while (area % W != 0) {
-            W--;
+    public String convertToBase7(int num) {
+        if(num == 0) return "0";
+        StringBuilder ans = new StringBuilder();
+        int temp = num;
+        if(num < 0)num = -num;
+        while(num >= 1){
+            ans.append((char)((num % 7) +'0'));
+            num = num/7;
         }
-        
-        int L = area / W;
-        
-        return new int[]{L, W};
+        ans = ans.reverse();
+        if(temp < 0){
+            ans.insert(0 , '-');
+        }
+        return ans.toString();
     }
 }
