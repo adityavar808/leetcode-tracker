236ab6527650258654e522b43fb51c26466fb360
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-09-08 22:43:33 +0530

    Create 1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers

---
+++ 1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers/1317-ConvertIntegertotheSumofTwoNo-ZeroIntegers.java
@@ -0,0 +1,26 @@
+// Last updated: 9/8/2025, 10:43:40 PM
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        int[] arr = new int[2];
+        for(int i = 1; i < n; i++){
+            int a = i;
+            int b = n - i;
+
+            if(areBothValid(a, b) == true){
+                arr[0] = a;
+                arr[1] = b;
+            }
+        }
+    return arr;
+    }
+    public static boolean checkZeroes(int a){
+            while (a > 0) {
+            if (a % 10 == 0) return false;
+            a /= 10;
+        }
+        return true;
+    }
+    public static boolean areBothValid(int a, int b) {
+        return checkZeroes(a) && checkZeroes(b);
+    }
+}
