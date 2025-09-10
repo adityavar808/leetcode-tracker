3a8f1cb468370eadf9677f3c4d76646e52e8ce87
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-09-10 12:25:36 +0530

    Create 2556-DisconnectPathinaBinaryMatrixbyatMostOneFlip

---
+++ 2556-DisconnectPathinaBinaryMatrixbyatMostOneFlip/2556-DisconnectPathinaBinaryMatrixbyatMostOneFlip.java
@@ -0,0 +1,21 @@
+// Last updated: 9/10/2025, 12:25:43 PM
+class Solution {
+    public boolean isPossibleToCutPath(int[][] grid) {
+        int first=dfs(grid,0,0);
+        grid[0][0]=1; 
+        int second=dfs(grid,0,0);
+        return second >= 1 ? false : true;
+    }
+    
+    private int dfs(int[][] g, int r, int c) {
+        if(r == g.length-1 && c == g[0].length-1)
+            return 1;
+        if(r == g.length || c == g[0].length || g[r][c] == 0)
+            return 0;
+        
+        g[r][c]=0;
+        if(dfs(g,r+1,c) == 1)
+            return 1;
+        return dfs(g,r,c+1);
+    }
+}
