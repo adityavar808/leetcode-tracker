bfad06f9957550871d9fc26bee0817afa49077fb
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-09-10 12:26:28 +0530

    Create 2432-NumberOfZeroFilledSubarrays

---
+++ 2432-NumberOfZeroFilledSubarrays/2432-NumberOfZeroFilledSubarrays.java
@@ -0,0 +1,28 @@
+// Last updated: 9/10/2025, 12:26:35 PM
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        int n=nums.length;
+        ArrayList<Integer> list=new ArrayList<>();
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1] && nums[i]==0){
+                count++;
+            }
+            else if(nums[i-1]==0){
+                list.add(count);
+                count=1;
+            }
+        }
+        if(nums[n-1]==0){
+            list.add(count);
+        }
+        long sum=0;
+        for(int num:list){
+            while(num>=1){
+                sum+=num;
+                num--;
+            }
+        }
+        return sum;
+    }
+}
