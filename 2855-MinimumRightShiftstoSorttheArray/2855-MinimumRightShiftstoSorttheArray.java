5febee23b95e85b711ba9378764c4f87ce8a9c1a
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-10-05 11:09:19 +0530

    Create 2855-MinimumRightShiftstoSorttheArray

---
+++ 2855-MinimumRightShiftstoSorttheArray/2855-MinimumRightShiftstoSorttheArray.java
@@ -0,0 +1,17 @@
+// Last updated: 10/5/2025, 11:09:18 AM
+   class Solution {
+          public int minimumRightShifts(List<Integer> nums) {
+              int ind = 0, c = 0;
+              for(int i = 1; i<nums.size(); i++){
+                  if(nums.get(i - 1) > nums.get(i)){ 
+                       ind = i;
+                       c++;
+                  }
+            }
+            if(c > 1){
+               return -1;
+           }
+           if(ind == 0){   return 0;  }
+           return nums.get(nums.size() - 1) > nums.get(0) ? -1 : nums.size() - ind; 
+           }
+       }
