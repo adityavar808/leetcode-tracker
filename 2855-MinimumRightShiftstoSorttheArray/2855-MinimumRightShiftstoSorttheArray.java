3505f2f453ef9bcb290930699ff46e2d06ac2f15
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-10-05 11:10:41 +0530

    File updated at 10/5/2025, 11:10:41 AM

--- 2855-MinimumRightShiftstoSorttheArray/2855-MinimumRightShiftstoSorttheArray.java
+++ 2855-MinimumRightShiftstoSorttheArray/2855-MinimumRightShiftstoSorttheArray.java
@@ -1,19 +1,40 @@
-// Last updated: 10/5/2025, 11:09:51 AM
+// Last updated: 10/5/2025, 11:10:41 AM
 class Solution {
-    public List<Integer> getRow(int rowIndex) {
-        List<Integer> row = new ArrayList<>();
-        row.add(1);
-
-        for (int i = 0; i < rowIndex; i++) {
-            List<Integer> newRow = new ArrayList<>();
-            newRow.add(1);
-            for (int j = 1; j < row.size(); j++) {
-                newRow.add(row.get(j - 1) + row.get(j));
-            }
-            newRow.add(1);
-            row = newRow;
-        }
-
-        return row;        
-    }
+	public boolean isAdditiveNumber(String num) {
+		long number1=0;
+		for(int i=0; i<num.length()-1; i++){
+			number1=number1*10+num.charAt(i)-'0';
+			long number2=0;
+			for(int j=i+1; j<num.length(); j++){
+				number2=number2*10+num.charAt(j)-'0';
+				if(solv(number1, number2, j+1, num, 2))
+					return true;
+				if(number2==0)
+					break;
+			}
+			if(number1==0)
+				break;
+		}
+		return false;
+	}
+	boolean solv(long number1, long number2, int curr, String num, int count) {
+		if(curr>=num.length()){
+			if(count>=3)
+				return true;
+			return false;
+		}
+		if(num.charAt(curr)=='0' && number1+number2!=0)
+			return false;
+    
+		long number=0;
+		long target=number1+number2;
+		for(int i=curr; i<num.length(); i++) {
+			number=number*10+num.charAt(i)-'0';
+			if(number==target && solv(number2, target, i+1, num, count+1))
+				return true;
+			else if(number>target)
+				break;
+		}
+		return false;
+	}
 }
