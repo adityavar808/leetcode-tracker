14019d13fa3fc99e8214f34385ea953e65907b37
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-10-11 19:04:28 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 10/11/2025, 7:04:29 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function() {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
