720d26d37aff9d4ae5622e9544063c8d42f74aec
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-10-11 19:04:22 +0530

    Create 2872-LargestElementInAnArrayAfterMergeOperations

---
+++ 2872-LargestElementInAnArrayAfterMergeOperations/2872-LargestElementInAnArrayAfterMergeOperations.java
@@ -0,0 +1,24 @@
+// Last updated: 10/11/2025, 7:04:24 PM
+class Solution {
+    public long maxArrayValue(int[] nums) {
+        ArrayList<Long> numero = new ArrayList();
+        for(int i=0;i<nums.length;i++){numero.add((long)nums[i]);}
+        int i = numero.size()-1;
+
+        while(i>0){
+            if(numero.get(i) < numero.get(i-1)){
+                i--;
+            }
+            else{
+                numero.set(i, (numero.get(i) + numero.get(i-1)));
+                numero.remove(i-1);
+                i--;
+            }
+        }
+        long max = 0;
+        for(int j=0;j<numero.size();j++){
+            max = max > numero.get(j) ? max : numero.get(i);
+        }
+        return max;
+    }
+}
