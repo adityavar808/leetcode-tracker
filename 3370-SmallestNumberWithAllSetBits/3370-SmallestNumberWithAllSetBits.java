74974f14456e51a75921119df93d55ee8e1a4ea2
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-10-29 11:40:40 +0530

    Create 3370-SmallestNumberWithAllSetBits

---
+++ 3370-SmallestNumberWithAllSetBits/3370-SmallestNumberWithAllSetBits.java
@@ -0,0 +1,10 @@
+// Last updated: 10/29/2025, 11:40:52 AM
+class Solution {
+    public int smallestNumber(int n) {
+        for (int i = 0; i <= n; i++) {
+            int z = 1 << i;
+            if (z > n) return z - 1;
+        }
+        return 0;
+    }
+}
