eeeab328367b6d2bc644b8f2667b41fd0c627633
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-11-08 02:02:56 +0530

    Create 3581-TheTwoSneakyNumbersOfDigitville

---
+++ 3581-TheTwoSneakyNumbersOfDigitville/3581-TheTwoSneakyNumbersOfDigitville.java
@@ -0,0 +1,23 @@
+// Last updated: 11/8/2025, 2:03:14 AM
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            if (numbers.contains(nums[i])) continue;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                numbers.add(nums[i]);
+            }
+        }
+        int[] result = new int[numbers.size()];
+        for (int i = 0; i < numbers.size(); i++) {
+            result[i] = numbers.get(i);
+        }
+        return result;
+    }
+}
