6cd6ca2ef6d0ba56d40f525275d3d104dce2400e
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-11-08 02:04:45 +0530

    Create 2137-FinalValueOfVariableAfterPerformingOperations

---
+++ 2137-FinalValueOfVariableAfterPerformingOperations/2137-FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,9 @@
+// Last updated: 11/8/2025, 2:05:02 AM
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (String op : operations)
+            x += 44 - op.charAt(1);        
+        return x;
+    }
+}
