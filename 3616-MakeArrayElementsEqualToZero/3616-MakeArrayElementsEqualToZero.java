838f4cadff2b860006f4940d48f8b4444a6c5f7a
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-11-08 02:02:38 +0530

    Create 3616-MakeArrayElementsEqualToZero

---
+++ 3616-MakeArrayElementsEqualToZero/3616-MakeArrayElementsEqualToZero.java
@@ -0,0 +1,15 @@
+// Last updated: 11/8/2025, 2:02:53 AM
+class Solution {
+    public int countValidSelections(int[] nums) {
+        int len = nums.length, count = 0, left = 0;
+        int right = Arrays.stream(nums).sum();
+        for (int i = 0; i < len; i++) {
+            left += nums[i];
+            right -= nums[i];
+            if (nums[i] != 0) continue;
+            if (left == right) count += 2;
+            if (Math.abs(left - right) == 1) count++;
+        }
+        return count;
+    }
+}
