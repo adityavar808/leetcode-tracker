c736d2c80a648f035db52678fd039e49d042b313
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-11-08 02:04:15 +0530

    Create 2650-SplitWithMinimumSum

---
+++ 2650-SplitWithMinimumSum/2650-SplitWithMinimumSum.java
@@ -0,0 +1,19 @@
+// Last updated: 11/8/2025, 2:04:32 AM
+class Solution {
+        public int splitNum(int num) {
+        char[] ca = Integer.toString(num).toCharArray();
+        Arrays.sort(ca);
+        int a = 0, b = 0;
+        for (int i = 0; i < ca.length; ++i) {
+            int digit = ca[i] - '0';
+            if (i % 2 == 0) {
+                a *= 10;
+                a += digit;
+            }else {
+                b *= 10;
+                b += digit;
+            }
+        }
+        return a + b;
+    }
+}
