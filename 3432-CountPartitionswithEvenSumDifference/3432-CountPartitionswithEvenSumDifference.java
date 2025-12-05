a1aeed12595d4abe035127a785166fabd8f33b1c
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-12-05 15:35:43 +0530

    File updated at 12/5/2025, 3:35:44 PM

--- 3432-CountPartitionswithEvenSumDifference/3432-CountPartitionswithEvenSumDifference.java
+++ 3432-CountPartitionswithEvenSumDifference/3432-CountPartitionswithEvenSumDifference.java
@@ -1,7 +1,22 @@
-// Last updated: 12/5/2025, 3:35:27 PM
+// Last updated: 12/5/2025, 3:35:44 PM
 1class Solution {
-2    public int countPartitions(int[] A) {
-3        int total = Arrays.stream(A).sum();
-4        return (total & 1) == 0 ? A.length - 1 : 0;
-5    }
-6}
+2    public int countPartitions(int[] nums) {
+3        int totalSum = 0;
+4        for (int i=0;i<nums.length;i++) {
+5            totalSum += nums[i]; 
+6        }
+7
+8        int leftSum = 0;
+9        int count = 0;
+10
+11        for (int i = 0; i < nums.length - 1; i++) { 
+12            leftSum += nums[i]; 
+13            int rightSum = totalSum - leftSum; 
+14  
+15            if ((leftSum % 2) == (rightSum % 2)) {
+16                count++; 
+17            }
+18        }
+19        return count; 
+20    }
+21    }
