985d3387255f1b2643b651dc1a4445e9d900ea45
Aditya Varshney <67576687+adityavar808@users.noreply.github.com>
2025-12-20 19:10:47 +0530

    Create 3493-MaximumNumberOfOperationsToMoveOnesToTheEnd

---
+++ 3493-MaximumNumberOfOperationsToMoveOnesToTheEnd/3493-MaximumNumberOfOperationsToMoveOnesToTheEnd.java
@@ -0,0 +1,13 @@
+// Last updated: 12/20/2025, 7:10:59 PM
+class Solution {
+    public int maxOperations(String s) {
+        int ones = 0, res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1')
+                ones++;
+            else if (i > 0 && s.charAt(i - 1) == '1')
+                res += ones;
+        }
+        return res;
+    }
+}
